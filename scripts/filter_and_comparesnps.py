85547626af7bb81d0920eb8f9a70c741a3ce802a
TSL-RamKrishna <ram_krishna.shrestha@tsl.ac.uk>
2019-02-04 11:48:48 +0000

    scripts moved to scripts folder and code updated.

---
+++ scripts/filter_and_comparesnps.py
@@ -0,0 +1,375 @@
+#!/usr/bin/env python3
+import os, sys, re
+import numpy as np
+import argparse
+import vcf
+
+rootPath=os.path.dirname(os.path.abspath(sys.argv[0]))
+sys.path.append(rootPath)
+
+from vcfFilter import filter
+
+parser=argparse.ArgumentParser(description="Script to filter the SNPs using user threshold values and compare the SNPs from multiple VCF files")
+
+parser.add_argument("--vcf", action="store", dest="vcf", nargs="+", help="Space separated vcf input files")
+parser.add_argument("--filter", action="store_true", dest="filter", default=False, help="Filter the SNPs")
+parser.add_argument("--compare", action="store_true", dest="compare", default=False, help="Compare the SNPs")
+parser.add_argument("--frequency", action="store", dest="frequency", default=70, type=int, help="Frequency of SNP call. Default: 70 [int]")
+parser.add_argument("--pvalue", action="store", dest="pvalue", default=0.05, type=float, help="Pvalue of the SNP call. Default: 0.05 [float]" )
+parser.add_argument("--genotype", action="store", dest="genotype", default="heterozygous", type=str, help="Genotype of the SNP call - heterozygous/homozygous/both. Default: heterozygous")
+parser.add_argument("--quality", action="store", dest="genotype_quality", default=10, type=int, help="Genotype quality of the SNP call. Default: 10")
+parser.add_argument("--rawreaddepth", action="store", dest="raw_read_depth", default=5, type=int, help="Raw read depth of the SNP call. Default: 5")
+parser.add_argument("--qualityreaddepth", action="store", dest="quality_read_depth", default=5, type=int, help="Quality read depth of the SNP call. Default: 5")
+parser.add_argument("--depthreference", action="store", dest="depth_in_reference", default=5, type=int, help="Depth in reference of the SNP call. Default: 5")
+parser.add_argument("--depthvariant", action="store", dest="depth_in_variant", default=5, type=int, help="Depth in variant of the SNP call. Default: 5")
+parser.add_argument("--show", action="store_true", dest="display", default=False, help="Display the results on the screen")
+parser.add_argument("--outdir", action="store", dest="outdir", default=os.path.abspath("."), help="Path to the output folder. Default: Current working directory")
+
+
+
+snpsites={}		# declare a dict type to store chromosome and snp positions from all vcfs
+snp_positions={}	# stores chromosome, position, ref and alt for each vcf file
+vcffilenames=[]
+
+
+class filter_vcf_records():
+	"""
+	a class to filter the vcf SNP records based on the thresholds provided
+	"""
+
+	def __init__(self, frequency=70, pvalue=0.05, genotype="heterozygous", genotype_quality=10, raw_read_depth=1, quality_read_depth=1, depth_in_reference=1, depth_in_variant=1):
+		""" Initializes the filter parameters with default values, if not provided
+			args:
+				:type frequency: int
+				:param frequency: Frequency of the ALT base in the SNP position
+
+				:type pvalue: float
+				:param pvalue: pvalue of the SNP call
+
+				:type genotype: string
+				:param genotype: genotype call heterozygous or homozygous of the SNP
+
+				:type genotype_quality: int
+				:param genotype_quality: quality value of the genotype
+
+				:type raw_read_depth: int
+				:param raw_read_depth: Raw read depth in the SNP call
+
+				:type quality_read_depth: int
+				:param quality_read_depth: Quality Read Depth of bases with Phred score >= 15
+
+				:type depth_in_reference: int
+				:param depth_in_reference: depth in reference supporting bases (read1)
+
+				:type depth_in_variant: int
+				:param depth_in_variant: depth in variant supporting bases (read2)
+			returns:
+				None
+		"""
+		self.filter=filter(frequency, pvalue, genotype, genotype_quality, raw_read_depth, quality_read_depth, depth_in_reference, depth_in_variant)
+	def set_filename(self, vcf):
+		"""
+			sets the VCF filename
+			:type vcf: string
+			:param vcf: VCF filename
+
+			returns:
+				None
+		"""
+		self.vcf=vcf
+
+	def set_record(self, record, samplename):
+		""" set a SNP record and record samplename
+		:type record: pyVCF object
+		:param record: a SNP record in a VCF file
+
+		:type samplename: string
+		:param samplename: name of a sample in the SNP call
+
+		returns:
+			None
+		"""
+		self.record=record
+		self.samplename = samplename
+
+	def filter_records(self):
+		""" (obsolete now) Filters all SNP records from a vcf filename
+			returns: list of pyvcf SNP objects that have passed the filter
+		"""
+		filter = self.filter.get_passed_filter_records(self.vcf, self.frequency, self.pvalue, self.genotype, self.genotype_quality, self.raw_read_depth, self.quality_read_depth, self.depth_in_reference, self.depth_in_variant)
+	def filter_a_record(self):
+		""" filters one SNP record at a time
+			returns:
+				True if the SNP record passes filter
+				False otherwise
+		"""
+		return self.filter.passed_filter(self.record, self.samplename)
+
+def filter_snps(do_filter=False):
+	"""
+		Filter the snps using threshold values as defined and populates the global variable to with SNP records
+
+		:type do_filter: boolean
+		:param do_filter: decision to filter the SNPs or not
+
+		return:
+			None
+	"""
+
+
+
+	def create_db_for_all_snps(chromosome, position ):
+		"""
+			records all chromosome and positions in global variable (dictionary data structure) and initializes with an array of False boolean values for each vcf input file
+			Each boolean value is a positional value of a snp in input vcf files in an array.
+			E.g. if input vcf files are ["test1.vcf", "test2.vcf", "test3.vcf"]
+			snpsites["chr1"]["1"] = [False, False, False]
+			snpsites["chr1"]["10"] = [False, False, False]
+			snpsites["chr2"]["1"] = [False, False, False]
+
+			:type chromosome: string
+			:param chromosome: name of the chromosome
+
+			:type position: int
+			:param position: position of SNP call in the chromosome
+
+			return:
+				None
+
+		"""
+
+		if  chromosome in snpsites.keys():
+			if str(position) in snpsites[chromosome].keys():
+				return
+			else:
+				snpsites[chromosome][str(position)] = [False] * len(vcffilenames)
+		else:
+			snpsites.update({chromosome:{str(position): [False] * len(vcffilenames) }})
+
+	def add_snp_to_database(filename, chromosome, position, ref, alt):
+		"""
+			append the snp records to the dictionary data structure once they passed the filter
+
+			:type filename: string
+			:param filename: vcf filename
+
+			:type chromosome: string
+			:param chromosome: chromosome name on which SNP was call
+
+			:type position: int
+			:param position: base position in the chromosome
+
+			:type ref: String
+			:param ref: reference base in SNP call
+
+			:type alt: String
+			:param alt: alternate base in SNP call
+
+			return:
+				None
+
+		"""
+
+		if filename in snp_positions.keys():
+			if chromosome in snp_positions[filename].keys():
+				snp_positions[filename][chromosome].update({str(position):{"ref":ref, "alt":str(alt).replace("[","").replace("]", "").replace(" ", "")}} )
+			else:
+				snp_positions[filename].update({chromosome:{str(position):{"ref":ref, "alt":str(alt).replace("[","").replace("]", "").replace(" ", "")}}})
+		else:
+			snp_positions.update({filename:{chromosome:{str(position) : {"ref": ref, "alt":str(alt).replace("[","").replace("]", "").replace(" ", "")}}}})
+
+	key_counter = 0
+	inputvcf = filter_vcf_records()	# class instance for filtering
+	for filename in  vcffilenames:
+		vcf_reader=vcf.Reader(open(filename), "rb")
+		samplename= vcf_reader.samples[0]
+		for record in vcf_reader:
+			chromosome, position, ref, alt = record.CHROM, record.POS, record.REF, record.ALT
+			position=str(position)
+
+			## code to build all snps position
+			create_db_for_all_snps(chromosome, position)
+
+			inputvcf.set_record(record, samplename)
+			filter_result = inputvcf.filter_a_record()
+			if do_filter==True:
+				if filter_result == True:
+					add_snp_to_database(filename, chromosome, position, ref, alt)
+					#snp_positions.update({str(key_counter) + "_" + chromosome + "_" + str(position):{"ref": str(ref), "alt":str(alt).replace("[","").replace("]", "")}})
+					snpsites[chromosome][str(position)][key_counter] = True
+				else:
+					pass
+
+			else:
+				add_snp_to_database(filename, chromosome, position, ref, alt)
+				#snp_positions.update({str(key_counter) + "_" + chromosome + "_" + str(position):{"ref": str(ref), "alt":str(alt).replace("[","").replace("]", "")}})
+				snpsites[chromosome][str(position)][key_counter]= True
+
+		key_counter+=1
+
+def count_list_elements_occurrences(alt_bases):
+	"""
+		counts number of each element of input array
+
+		:type alt_bases: Array
+		:param alt_bases: alternate bases from all VCF files for same chromosome and position. e.g. ["A", "T", "A", "T,C"]
+
+		return:
+			array with count of each element in the input array. e.g for above array it retuns [2, 1, 2, 1]
+
+	"""
+	counts=[]
+	for x in alt_bases:
+		counts.append(alt_bases.count(x))
+	return counts
+
+def get_unique_snps():
+	""" records a unique snps in a vcf file """
+
+	key_counter = 0
+
+	for chromosome in snpsites.keys():
+		for position in snpsites[chromosome].keys():
+			if snpsites[chromosome][position][key_counter] == True and sum(snpsites[chromosome][position]) == 1:  # First any(array) finds first True and second any(array) finds another True, if second True, it will say False
+				# This is unique snp
+
+				#vcf_database[key]["unique_snps"].append([chromosome, position, vcf_database[key]["snp_positions"][chromosome + "_" + position]["ref"], ",".join(vcf_database[key]["snp_positions"][chromosome + "_" + position]["alt"]) ])
+				snp_positions[vcffilenames[key_counter]][chromosome][position].update({"unique":True})
+			elif sum(snpsites[chromosome][position]) >=2:		# there might be snp at same position but with different alt base
+
+				snp_index = [i for i, j in enumerate(snpsites[chromosome][position]) if j==True]
+
+				totalindex = len(snp_index)
+				# Lets check the alt base in these vcf files using index
+				# lets get array of alt bases from each file
+				alt_snps=[]
+				for index in snp_index:
+					alt_snps.append(snp_positions[vcffilenames[index]][chromosome][position]["alt"])
+
+				# get the counts of the elements
+
+				counts = count_list_elements_occurrences(alt_snps)
+
+				for index in range(len(counts)):
+					if counts[index] == 1:
+						# this is unique, so occurred once
+						snp_positions[vcffilenames[snp_index[index]]][chromosome][position].update({"unique":True}) # vcffilenames[snp_index[index]] =  this will be the filename
+						#print("this is unique", vcffilenames[snp_index[index]], chromosome, position, snp_positions[vcffilenames[snp_index[index]]][chromosome][position])
+
+
+			#else:
+			#	vcf_database["snp_positions"][chromosome + "_" + position].update({"unique":False})
+	key_counter+=1
+	return
+
+def get_common_snps():
+
+	""" records SNPs common to all VCF input files """
+
+	for chromosome in snpsites.keys():
+		for position in snpsites[chromosome].keys():
+			if all(snpsites[chromosome][position]) == True:
+				#lets check if all alt bases are same
+				alt_snps=[]
+				for index in range(len(snpsites[chromosome][position])):
+					alt_snps.append(snp_positions[vcffilenames[index]][chromosome][position]["alt"])
+
+				counts = count_list_elements_occurrences(alt_snps)
+
+				for countindex in range(len(counts)):
+					if counts[countindex] == len(vcffilenames):
+							snp_positions[vcffilenames[countindex]][chromosome][position].update({"common" : True})
+
+
+def save_display_common_unique_snps(outdir, save=True, display=False):
+	"""
+		save the common/unique snps to files and/or display the results
+
+		:type outdir: string
+		:param outdir: output directory to save the output files
+
+		:type save: boolean
+		:param save: save the results to output files. Default True
+
+		:type display: boolean
+		:param display: display the results on the screen. Default False
+
+		returns:
+			None
+	"""
+	outfiles=[]
+	for filename in vcffilenames:
+		outfile=outdir + "/" + os.path.basename(filename).replace(".vcf", "") + "_snpanalysis.txt"
+		outfh=open(outfile, "w"); outfiles.append(outfile)
+		if display == True:
+			print("Common and Unique SNPS in vcf File : ", filename)
+		else:
+			print("Common and Unique SNPs from file ", filename , " are saved in :", outfile)
+		for chromosome in snp_positions[filename].keys():
+			for position in snp_positions[filename][chromosome].keys():
+				if "common" in snp_positions[filename][chromosome][position].keys() and snp_positions[filename][chromosome][position]["common"] == True:
+					if save == True:
+						outfh.write(" ".join([chromosome,position, snp_positions[filename][chromosome][position]["ref"], snp_positions[filename][chromosome][position]["alt"], "common"]) + "\n")
+					if display == True:
+						print(" ".join([chromosome,position, snp_positions[filename][chromosome][position]["ref"], snp_positions[filename][chromosome][position]["alt"], "common"]))
+				elif "unique" in snp_positions[filename][chromosome][position].keys() and snp_positions[filename][chromosome][position]["unique"] == True:
+					if save == True:
+						outfh.write(" ".join([chromosome,position, snp_positions[filename][chromosome][position]["ref"], snp_positions[filename][chromosome][position]["alt"], "unique"]) + "\n")
+					if display == True:
+						print(" ".join([chromosome, position, snp_positions[filename][chromosome][position]["ref"], snp_positions[filename][chromosome][position]["alt"], "unique"]))
+
+		outfh.close()
+	if display==True:
+		print("The outputs are saved in these files :", " ".join(outfiles))
+
+	return
+
+def main(outdir, display, filter=True, compare=False):
+
+	""" the main function to filter SNPs, compare SNPs and to display/save the common and unique SNPs
+		:type outdir: String
+		:param outdir: output directory to save the output files
+
+		:type display: boolean
+		:param display: display results on the screen
+
+		:type filter: boolean
+		:param filter: Filter SNPs. Default True
+
+		:type compare: boolean
+		:param compare: compare SNPs. Default False
+
+		returns:
+			None
+	"""
+
+	if filter == True:
+		filter_snps(True)
+	else:
+		filter_snps(False)
+
+	if compare == True:
+		get_unique_snps()
+		get_common_snps()
+
+	save_display_common_unique_snps(outdir, save=True, display=display)
+
+if  __name__ == "__main__":
+
+
+	options=parser.parse_args()
+	if not options.vcf:
+		print("Input VCF file/s not provided. Use option --vcf to provde the input VCF files (mutliple vcf files should be space separated)")
+		exit(1)
+	if len(options.vcf) ==1 and options.compare == True:
+		print("Number of VCF files provided : ", len(options.vcf))
+		print("SNP compare will not be done.")
+		options.compare=False
+
+
+
+	vcffilenames  = options.vcf
+
+
+	main(options.outdir, options.display, options.filter, options.compare)
+	exit(0)
---
+++ scripts/vcfFilter.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+import sys, re
+import numpy as np
+import argparse
+import vcf
+#############################
+### program to filter VCF snps with following filter criteria
+### 1) snp position should not have another snp 200 bp forward or backward
+### 2) min depth = 100
+#############################
+### usage : python scriptname refseq.lengths vcf.filename outputfilename
+# parser=argparse.ArgumentParser(description="Filter vcf")
+# parser.add_argument("-i", "--input", dest="input", action="store", help="VCF input file")
+# parser.add_argument("--gt", dest="genotype", action="store", default="homozygous", help="Genotype of the SNP")
+# parser.add_argument("--freq", dest="frequency", action="store", type=int, default= 50, help="frequency of the SNP")
+# parser.add_argument("--dp", dest="quality_read_depth", action="store", type=int, default=5, help="Filter SNPs with quality read depth")
+# parser.add_argument("--pvalue", dest="pvalue", action="store", type=float, default=0.05, help="Filter SNPs with pvalue of snp call")
+# parser.add_argument('--output', '--out', dest='output', action='store', default="vcffilter_output.vcf", help="Output filename")
+# parser.add_argument('--gq', dest='genotype_quality', type=int, action='store', default=10, help='Genotype Quality')
+# parser.add_argument('--rrd','--sdp', dest='raw_read_depth', type=int, action='store', default=3, help='Raw read depth as reported by Samtools')
+# parser.add_argument('--rd', dest='depth_in_reference', type=int, action='store', default=3, help='Raw depth in reference')
+# parser.add_argument('--ad', dest='depth_in_variant', type=int, action='store', default=3, help='Raw depth in varaint')
+#
+# options = parser.parse_args()
+
+
+class filter():
+	'''	A class for filtering SNPs in VCF files	'''
+	def __init__(self, frequency=70, pvalue=0.05, genotype='heterozygous', genotype_quality=20, raw_read_depth=5, quality_read_depth=5, depth_in_reference=5, depth_in_variant=5):
+		"""
+		Initializes the filter threshold values
+		"""
+		#self.vcffilename = vcffilename
+		self.frequency=frequency
+		self.pvalue=pvalue
+		self.genotype=genotype
+		self.genotype_quality=genotype_quality
+		self.raw_read_depth=raw_read_depth
+		self.quality_read_depth=quality_read_depth
+		self.depth_in_reference=depth_in_reference
+		self.depth_in_variant=depth_in_variant
+
+	def get_a_record(self):
+		"""
+		Gets the next snp record
+		"""
+		self.count_records+=1
+		return self.vcf_reader.next()
+	def get_samplename(self):
+		""" returns the samplename of the snp record """
+		return self.vcf_records.samples[0]
+	def get_record_calldata(self, record):
+		""" gets a snp record call data from pyVCF object"""
+		self.recordcall = record.genotype(self.sample)
+		return self.recordcall
+
+	def get_frequency(self, record, sample):
+		'return frequency of SNP genotype'
+		call = record.genotype(sample)
+		return float(call.data.FREQ.replace('%', ''))
+
+	def get_pvalue(self,record, sample):
+		'return pvalue of a SNP record'
+		call = record.genotype(sample)
+		return call.data.PVAL
+	def get_genotype(self,record, sample):
+		'return the genotype of a snp record - heterozygous or homozygous'
+		call=record.genotype(sample)
+		genotype=call.data.GT
+		array=genotype.split('/')
+		if array[0] == array[1]:
+			return 'homozygous'
+		else:
+			return 'heterozygous'
+
+	def get_quality_read_depth(self,record, sample):
+		'return the quality read depth for snp record'
+		call=record.genotype(sample)
+		return int(call.data.DP)
+	def get_genotype_quality(self, record, sample):
+		'return genotype quality'
+		call=record.genotype(sample)
+		return int(call.data.GQ)
+	# def get_avg_sample_depth(self, record, sample):
+	# 	call=record.genotype(sample)
+	# 	return int(call.data.ADP)
+	def get_raw_read_depth(self, record, sample):
+		""" returns raw read depth of the snp record"""
+		call=record.genotype(sample)
+		return int(call.data.SDP)
+	def get_depth_in_reference(self, record, sample):
+		""" returns depth in reference data for the snp record"""
+		call=record.genotype(sample)
+		return int(call.data.RD)
+	def get_depth_in_variant(self, record, sample):
+		""" returns depth in variant data for the snp record """
+		call=record.genotype(sample)
+		return int(call.data.AD)
+	def check_threshold_value(self, recordvalue, testvalue):
+		""" function to test snp record value to threshold value
+		args:
+		recordvalue - value in a snp record
+		testvalue - threshold value to test
+
+		returns:
+		True - if condition is True
+		False - if condition is False
+		"""
+
+		if recordvalue >= testvalue:
+			return True
+		else:
+			return False
+
+	def check_frequency(self,record, sample, testvalue):
+		""" function to check frequency of a base in snp call"""
+
+		return self.check_threshold_value(self.get_frequency(record,sample), testvalue)
+	def check_pvalue(self, record, sample,testvalue):
+		""" function to check pvalue of alt base call in a snp call"""
+		record_pvalue=float(self.get_pvalue(record, sample))
+		if record_pvalue <= float(testvalue):
+			return True
+		else:
+			return False
+		#return (not self.check_threshold_value(float(self.get_pvalue(record, sample)), np.float32(testvalue)))		# here we are testing pvalue less or equal to, so we have to return the opposite result
+	def check_genotype(self,record, sample,testvalue):
+		""" function to check genotype of alt base in the snp call"""
+		return self.check_threshold_value(self.get_genotype(record, sample), testvalue)
+	def check_genotype_quality(self,record, sample,testvalue):
+		""" function to check genotype quality of alt base in the snp call"""
+		return self.check_threshold_value(self.get_genotype_quality(record, sample), testvalue)
+	def check_quality_read_depth(self, record, sample,testvalue):
+		""" function to call read depth quality of alt base in the snp call"""
+		return self.check_threshold_value(self.get_quality_read_depth(record, sample), testvalue)
+	def check_raw_read_depth(self,record, sample,testvalue):
+		""" function to check raw read depth of alt base in the snp call"""
+		return self.check_threshold_value(self.get_raw_read_depth(record,sample), testvalue)
+	def check_depth_in_reference(self,record, sample,testvalue):
+		""" function to check dpeth in reference for alt base call in the snp call"""
+		return self.check_threshold_value(self.get_depth_in_reference(record, sample), testvalue)
+	def check_depth_in_variant(self,record, sample,testvalue):
+		""" function to check depth in variant for alt base in the snp call"""
+		return self.check_threshold_value(self.get_depth_in_variant(record, sample), testvalue)
+
+
+	def passed_filter(self, record, samplename):
+		"""
+		function to test if a snp record passed the filter or not
+		args:
+			record - pyVCF SNP record object
+			samplename - samplename in the SNP record
+
+		"""
+		results=[
+				self.check_frequency(record, samplename, self.frequency),
+				self.check_pvalue(record, samplename, self.pvalue),
+				self.check_genotype(record,samplename, self.genotype),
+				self.check_genotype_quality(record,samplename, self.genotype_quality),
+				self.check_quality_read_depth(record, samplename, self.quality_read_depth),
+				self.check_raw_read_depth(record,samplename, self.raw_read_depth),
+				self.check_depth_in_reference(record, samplename, self.depth_in_reference),
+				self.check_depth_in_variant(record, samplename, self.depth_in_variant)
+				]
+
+		if all(results):      # also can be used if results.count(True) == 8
+			return True
+		else:
+			return False
+
+	def get_passed_filter_records(self):
+		"""
+			function to filter all SNP records from a VCF file
+		"""
+
+		records_passed=[]
+		samplename=self.get_samplename()
+		for record in self.vcf_records:
+			if self.passed_filter(record, samplename) == True:
+				records_passed.append(record)
+
+		return records_passed
+
+
+		#test_functions=['check_frequency', 'check_pvalue', 'check_genotype', 'check_genotype_quality', 'check_quality_read_depth', 'check_raw_read_depth', 'check_raw_depth_in_reference', 'check_depth_in_variant']
+
+
+
+if __name__=='__main__':
+	vcffilter=filter(options.input)
+	vcffilter.open_vcf_writer(options.output)
+	samplenames=vcffilter.samplenames
+
+	for record in vcffilter.vcf_reader:
+		for samplename in samplenames:
+			if vcffilter.do_filter(record, samplename, options.frequency, options.pvalue, options.genotype, options.genotype_quality, options.raw_read_depth, options.quality_read_depth,  options.depth_in_reference, options.depth_in_variant) == True:
+				print (record)
